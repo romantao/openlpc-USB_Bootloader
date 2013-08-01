e70a2decfd84103b936d27a3b3fdfa77f6fcaffd
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-08-01 14:28:28 -0400

    Disable logging unless DEBUG=1 when compiling.

--- src/log.c
+++ src/log.c
@@ -8,6 +8,7 @@
 const int MAX_LOG_LINE_LENGTH = 120;
 
 void debug_no_newline(const char* format, ...) {
+#ifdef __DEBUG__
     va_list args;
     va_start(args, format);
     char buffer[MAX_LOG_LINE_LENGTH];
@@ -15,6 +16,7 @@ void debug_no_newline(const char* format, ...) {
 
     _printf(buffer);
     va_end(args);
+#endif // __DEBUG__
 }
 
 void initialize_logging() {
